6c8a295fd4c3b1c3b1846f2b51d7c8c445049e3c
Ruchi Tarale <80529830+ruchi-05@users.noreply.github.com>
2021-04-02 12:59:01 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import About from './components/About'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+
+  const App = () => {
+    const [showAddTask, setShowAddTask] = useState(false)
+    const [tasks, setTasks] = useState([
+      {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Meeting at school',
+        day: 'Feb 6th at 1:30',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 7th at 7:30',
+        reminder: false,
+      },
+    ])
+
+    // Add Task
+    const addTask = (task) => {
+      const id = Math.floor(Math.random() * 10000) + 1
+      const newTask = { id, ...task }
+      setTasks([...tasks, newTask])
+    }
+
+    // Delete task
+    const deleteTask = (id) => {
+      setTasks(tasks.filter((task) => task.id !== id))
+    }
+
+    // Toggle Reminder
+    const toggleReminder = (id) => {
+      setTasks(
+        tasks.map((task) => task.id === id
+          ? { ...task, reminder: !task.reminder } : task
+        )
+      )
+    }
+
+
+    return (
+      <div className='container'>
+        <Header onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask} />
+        {showAddTask && <AddTask onAdd={addTask} />}
+        {tasks.length > 0 ? (<Tasks tasks={tasks}
+          onDelete={deleteTask} onToggle={toggleReminder} />)
+          : ('No Tasks To Show!')}
+
+        <Footer />
+      <About />
+      </div>
+
+    )
+  }
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import About from "./About"
+
+const footer = () => {
+    return (
+        <footer>
+            <p>Copyright &copy; Ruchi tarale 2021</p>
+            
+        </footer>
+    )
+}
+
+export default footer
